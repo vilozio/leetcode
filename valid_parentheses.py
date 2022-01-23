a1258f09e32f3ad2b59564019b8ee88b0dfb3ec0
vilozio <korotkovkm@gmail.com>
2022-01-23 13:05:03 +0000

    Valid parentheses best

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -1,44 +1,33 @@
-from dataclasses import dataclass, field
-from typing import List, Dict, Tuple
-
-
-@dataclass
-class Bracket:
-    stack: List[int] = field(default_factory=list)
-    counter: int = 0
-    threshold: int = 0
-
-
 class Solution:
     def isValid(self, s: str) -> bool:
-        brackets: Dict[Tuple, Bracket] = {
-            ('{', '}'): Bracket(),
-            ('[', ']'): Bracket(),
-            ('(', ')'): Bracket(),
-        }
-        for c in s:
-            for bracket_key in brackets.keys():  # search for a bracket type
-                if c in bracket_key:
-                    bracket = brackets[bracket_key]
-                    rest_brackets = [b for k, b in brackets.items() if k != bracket_key]
-                    if c == bracket_key[0]:
-                        bracket.counter += 1
-                        for other_bracket in rest_brackets:
-                            other_bracket.stack.append(other_bracket.threshold)
-                            other_bracket.threshold = other_bracket.counter
-                    if c == bracket_key[1]:
-                        bracket.counter -= 1
-                        if bracket.counter < bracket.threshold:
-                            return False
-                        for other_bracket in rest_brackets:
-                            if other_bracket.counter != other_bracket.threshold:
-                                return False
-                            if other_bracket.stack:
-                                other_bracket.threshold = other_bracket.stack.pop()
-                    break  # found the bracket type, stop search
-        if any(b.counter != 0 for b in brackets.values()):
-            return False
-        return True
+        # The stack to keep track of opening brackets.
+        stack = []
+
+        # Hash map for keeping track of mappings. This keeps the code very clean.
+        # Also makes adding more types of parenthesis easier
+        mapping = {")": "(", "}": "{", "]": "["}
+
+        # For every bracket in the expression.
+        for char in s:
+
+            # If the character is an closing bracket
+            if char in mapping:
+
+                # Pop the topmost element from the stack, if it is non empty
+                # Otherwise assign a dummy value of '#' to the top_element variable
+                top_element = stack.pop() if stack else '#'
+
+                # The mapping for the opening bracket in our hash and the top
+                # element of the stack don't match, return False
+                if mapping[char] != top_element:
+                    return False
+            else:
+                # We have an opening bracket, simply push it onto the stack.
+                stack.append(char)
+
+        # In the end, if the stack is empty, then we have a valid expression.
+        # The stack won't be empty for cases like ((()
+        return not stack
 
 
 def call(string):
