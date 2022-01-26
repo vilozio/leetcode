a651a95e68ae575447335f35e9f09bcfb22884f4
vilozio <korotkovkm@gmail.com>
2022-01-26 18:44:57 +0000

    189 Rotate array best

--- rotate_array_189.py
+++ rotate_array_189.py
@@ -2,46 +2,22 @@ from typing import List
 
 
 class Solution:
-    def swap(self, nums, from_indices, to_indices):
-        i, j = from_indices[0], to_indices[0]
-        while i <= from_indices[1] and j <= to_indices[1]:
-            nums[i], nums[j] = nums[j], nums[i]
-            i += 1
-            j += 1
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k %= n
 
-    def rotate(self, nums: List[int], k: int, from_i=0, to_i=None, to_right=True) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        if k == 0:
-            return
-        if to_i is None:
-            to_i = len(nums) - 1
-        n = to_i - from_i + 1
-        if n // 2 >= k:
-            for i in range(n // k):
-                if to_right:
-                    from_indices = (from_i + i * k, from_i + i * k + k - 1)
-                    to_indices = (to_i - k + 1, to_i)
-                    if from_indices[1] >= to_indices[0]:
-                        from_indices = (from_indices[0], to_indices[0] - 1)
-                else:
-                    from_indices = (from_i, from_i + k - 1)
-                    to_indices = (to_i - i * k - k + 1, to_i - i * k)
-                    if from_indices[1] >= to_indices[0]:
-                        to_indices = (from_indices[1] + 1, to_indices[1])
-                len_from = from_indices[1] - from_indices[0] + 1
-                len_to = to_indices[1] - to_indices[0] + 1
-                if len_from == len_to:
-                    self.swap(nums, from_indices, to_indices)
-                else:
-                    self.rotate(nums, len_from if to_right else len_to,
-                                min(from_indices[0], to_indices[0]),
-                                max(from_indices[1], to_indices[1]), not to_right)
-        elif k < n:
-            self.rotate(nums, n - k, from_i, to_i, not to_right)
-        else:
-            self.rotate(nums, k % n, from_i, to_i, to_right)
+        start = count = 0
+        while count < n:
+            current, prev = start, nums[start]
+            while True:
+                next_idx = (current + k) % n
+                nums[next_idx], prev = prev, nums[next_idx]
+                current = next_idx
+                count += 1
+
+                if start == current:
+                    break
+            start += 1
 
 
 def testcase(nums, k, result):
