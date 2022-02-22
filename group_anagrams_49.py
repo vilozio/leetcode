ba8864e7364d1e438cfc56033fc541923bd3b051
vilozio <korotkovkm@gmail.com>
2022-02-22 22:31:46 +0000

    49 Group Anagrams best

--- group_anagrams_49.py
+++ group_anagrams_49.py
@@ -5,5 +5,8 @@ class Solution(object):
     def groupAnagrams(self, strs):
         ans = collections.defaultdict(list)
         for s in strs:
-            ans[tuple(sorted(s))].append(s)
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            ans[tuple(count)].append(s)
         return ans.values()
