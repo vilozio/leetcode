6813092c267fd8edd01b7036fc6b46f35cc84a51
vilozio <korotkovkm@gmail.com>
2022-02-26 21:42:52 +0000

    252. Meeting Rooms solution

---
+++ meeting_rooms_252.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if not intervals:
+            return True
+        intervals.sort(key=lambda x: x[0])
+        for i, (_, i_end) in enumerate(intervals[:-1]):
+            if i_end > intervals[i + 1][0]:
+                return False
+        return True
+
+# [[0, 10], [11, 12], [20, 20]]
