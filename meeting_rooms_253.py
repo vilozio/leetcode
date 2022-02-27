c7dfc8804e385cee808fc2d154485310c1e1e947
vilozio <korotkovkm@gmail.com>
2022-02-27 16:22:48 +0000

    253. Meeting Rooms II solution

--- meeting_rooms_253.py
+++ meeting_rooms_253.py
@@ -1,25 +1,19 @@
+import heapq
 from typing import List
 
 
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        min_rooms = curr_rooms = 1  # 1, 2
-        max_interval_end = 0  # 36
-        min_interval_end = float('inf')  # 4
+        heap = []
+        min_rooms = 1
         intervals.sort(key=lambda x: x[0])
-        for i, (_, i_end) in enumerate(intervals[:-1]):  # 34
-            if min(i_end, min_interval_end) > intervals[i + 1][0]:  # 16, 20
-                curr_rooms += 1
-                max_interval_end = max(i_end, max_interval_end, intervals[i + 1][1])
-                min_interval_end = min(i_end, min_interval_end, intervals[i + 1][1])
-            elif max(i_end, max_interval_end) > intervals[i + 1][0]:
-                min_interval_end = min(i_end, intervals[i + 1][1])
-            else:
-                min_rooms = max(min_rooms, curr_rooms)
-                curr_rooms = 1
-                max_interval_end = 0
-                min_interval_end = float('inf')
-        return max(min_rooms, curr_rooms)
+        heapq.heappush(heap, (intervals[0][1], intervals[0]))  # [(36, [2, 36])]
+        for intr in intervals[1:]:  # [39, 46]
+            if intr[0] >= heap[0][0]:  # 39 >= 20
+                heapq.heappop(heap)  # [(34, [13, 34]), (36, [2, 36])]
+            heapq.heappush(heap, (intr[1], intr))  # [(46, [39, 46]), (34, [13, 34]), (36, [2, 36])]
+            min_rooms = max(min_rooms, len(heap))  # 3
+        return min_rooms
 
 # [[0, 1], [1, 5], [4, 5], [4, 10], [11, 12]]
 # [[1, 3], [2, 10], [4, 6], [5, 7], [6, 8]]
