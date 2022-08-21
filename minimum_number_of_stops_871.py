f6200b6557c5c496598e8d594a0b54dfb6a81cb4
Cyril Korotkov <korotkovkm@gmail.com>
2022-08-21 13:35:28 +0200

    871. Minimum Number of Refueling Stops
    Solution with time O(N^2).

--- minimum_number_of_stops_871.py
+++ minimum_number_of_stops_871.py
@@ -2,29 +2,21 @@ class Solution:
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
         if startFuel >= target:
             return 0
-        stations.append([target, float('inf')])
-        dp = [(startFuel, 0)]
-        for station_position, station_fuel in stations:
-            min_stops = float('inf')
-            max_fuel = float('-inf')
-            for state_fuel, state_stops in dp:
-                if state_fuel >= station_position and state_stops <= min_stops:
-                    if state_stops == min_stops:
-                        max_fuel = max(max_fuel, state_fuel)
-                    else:
-                        max_fuel = state_fuel
-                        min_stops = state_stops
-            if min_stops == float('inf'):  # Station is not reachable.
+        elif not stations:
+            return -1
+        dp = [startFuel] + [-1 for _ in range(len(stations))]
+        for station_i, (station_position, station_fuel) in enumerate(stations):
+            new_state = []
+            for max_fuel in dp[:station_i + 1]:
+                if max_fuel >= station_position:
+                    new_state.append(max_fuel + station_fuel)
+                else:
+                    new_state.append(-1)
+            if sum(new_state) == -len(new_state):  # Station is not reachable.
                 return -1
-            dp.append((max_fuel + station_fuel, min_stops + 1))
-        return dp[-1][1] - 1  # Last station stop doesn't count.
-
-    
-# target = 100, startFuel = 10, stations = [[10, 50], [20, 10], [50, 70]]
-#
-# stations  = [[10, 50], [20, 10], [50, 70], [100, inf]]
-# dp        = [(10, 0), (60, 1), (70, 2), (130, 2)]
-# station_position, station_fuel = 20, 10
-# min_stops = 1
-# max_fuel  = 60
-# state.fuel, state.stops = 60, 1
+            for state_i, new_fuel in enumerate(new_state):
+                dp[state_i + 1] = max(dp[state_i + 1], new_fuel)
+        for stops, fuel in enumerate(dp):
+            if fuel >= target:
+                return stops
+        return -1
